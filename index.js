93edf2abe2190b0b24c91f3c234b2ae2b39ce445
Loren Cipriano <loren25cipriano@gmail.com>
2021-06-21 10:43:34 -0700

    commit tree a925ada3b375e7ac0d80b3151301cbcd26373ce6

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+
+// Define a Polygon class. Polygon will accept an Array of integers as a parameter, 
+// which will represent each side of a shape.
+
+class Polygon {
+    constructor(side) {
+        this.side = side
+    }
+
+    get countSides(){
+        return this.side.length
+    }
+
+    get perimeter() {
+        return this.side.length * 5
+    }  
+}
+
+// FINISH LATER 
+
+class Triangle extends Polygon {
+    
+    get isValid() {
+
+    }
+}
